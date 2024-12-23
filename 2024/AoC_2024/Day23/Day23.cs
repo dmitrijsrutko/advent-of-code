ff024efac37d53899f7de2c660868bc82b163f34
Dmitrijs Rutko <>
2024-12-23 20:00:01 +0200

    Day 23. Max clique by BronKerbosch1 and BronKerbosch2 (with pivoting)

--- 2024/AoC_2024/Day23/Day23.cs
+++ 2024/AoC_2024/Day23/Day23.cs
@@ -84,6 +84,108 @@ namespace AoC_2024
             return MaxSubGraph + " :: " + pwd;
         }
 
+        private static long MaxClique = 0;
+
+        /*
+         * 
+            algorithm BronKerbosch1(R, P, X) is
+                if P and X are both empty then
+                    report R as a maximal clique
+                for each vertex v in P do
+                    BronKerbosch1(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+                    P := P \ {v}
+                    X := X ⋃ {v}
+        * */
+        private static void BronKerbosch1(HashSet<string> R, HashSet<string> P, HashSet<string> X, Dictionary<string, HashSet<string>> N)
+        {
+            if ((P.Count == 0) && (X.Count == 0))
+            {
+                if (R.Count > MaxClique)
+                {
+                    MaxClique = R.Count;
+
+                    SortedSet<string> sorted = new SortedSet<string>(R);
+                    string key = string.Join(',', sorted);
+                    Console.WriteLine(MaxClique + " :: " + key);
+                }
+
+                return;
+            }
+
+            foreach (string v in P)
+            {
+                HashSet<string> RX = new HashSet<string>(R.Union(new string[] { v }));
+                HashSet<string> PX = new HashSet<string>(P.Intersect(N[v]));
+                HashSet<string> XX = new HashSet<string>(X.Intersect(N[v]));
+
+                BronKerbosch1(RX, PX, XX, N);
+
+                P.Remove(v);
+                X.Add(v);
+            }
+        }
+
+        /*
+         * 
+            algorithm BronKerbosch2(R, P, X) is
+                if P and X are both empty then
+                    report R as a maximal clique
+                choose a pivot vertex u in P ⋃ X
+                for each vertex v in P \ N(u) do
+                    BronKerbosch2(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+                    P := P \ {v}
+                    X := X ⋃ {v}         
+         * */
+        private static void BronKerbosch2(HashSet<string> R, HashSet<string> P, HashSet<string> X, Dictionary<string, HashSet<string>> N)
+        {
+            if ((P.Count == 0) && (X.Count == 0))
+            {
+                if (R.Count > MaxClique)
+                {
+                    MaxClique = R.Count;
+
+                    SortedSet<string> sorted = new SortedSet<string>(R);
+                    string key = string.Join(',', sorted);
+                    Console.WriteLine(MaxClique + " :: " + key);
+                }
+
+                return;
+            }
+                        
+            string u = P.Union(X).First();
+            foreach (string v in P.Except(N[u]))
+            {
+                HashSet<string> RX = new HashSet<string>(R.Union(new string[] { v }));
+                HashSet<string> PX = new HashSet<string>(P.Intersect(N[v]));
+                HashSet<string> XX = new HashSet<string>(X.Intersect(N[v]));
+
+                BronKerbosch2(RX, PX, XX, N);
+
+                P.Remove(v);
+                X.Add(v);
+            }
+        }
+
+        private static void Solve03(Dictionary<string, HashSet<string>> connections)
+        {
+            // The recursion is initiated by setting R and X to be the empty set and P to be the vertex set of the graph.
+            HashSet<string> R = new HashSet<string>();
+            HashSet<string> P = new HashSet<string>(connections.Keys);
+            HashSet<string> X = new HashSet<string>();
+
+            BronKerbosch1(R, P, X, connections);
+        }
+
+        private static void Solve04(Dictionary<string, HashSet<string>> connections)
+        {
+            // The recursion is initiated by setting R and X to be the empty set and P to be the vertex set of the graph.
+            HashSet<string> R = new HashSet<string>();
+            HashSet<string> P = new HashSet<string>(connections.Keys);
+            HashSet<string> X = new HashSet<string>();
+
+            BronKerbosch2(R, P, X, connections);
+        }
+
         public static void Run()
         {
             string day = "23";
@@ -120,11 +222,14 @@ namespace AoC_2024
 
             long started = Environment.TickCount;
 
-            long solve01 = Solve01(connections);
-            Console.WriteLine(solve01);
+            //long solve01 = Solve01(connections);
+            //Console.WriteLine(solve01);
 
-            string solve02 = Solve02(connections);
-            Console.WriteLine(solve02);
+            //string solve02 = Solve02(connections);
+            //Console.WriteLine(solve02);
+
+            //Solve03(connections);
+            Solve04(connections);
 
             long elapsed = Environment.TickCount - started;
             Console.WriteLine();
@@ -140,5 +245,20 @@ namespace AoC_2024
 13 :: bw,dr,du,ha,mm,ov,pj,qh,tz,uv,vq,wq,xw
 
 Elapsed: 1581 ms
+
+
+Max clique by BronKerbosch1
+
+12 :: cn,hx,jq,jr,mg,rk,sk,sl,sv,tg,vn,xv
+13 :: bw,dr,du,ha,mm,ov,pj,qh,tz,uv,vq,wq,xw
+
+Elapsed: 466 ms
+
+Max clique by BronKerbosch2 (with pivoting)
+
+12 :: cn,hx,jq,jr,mg,rk,sk,sl,sv,tg,vn,xv
+13 :: bw,dr,du,ha,mm,ov,pj,qh,tz,uv,vq,wq,xw
+
+Elapsed: 41 ms
 
  * */
